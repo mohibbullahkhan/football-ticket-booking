6e4991cf3eaafb0a66e5b68cefd34e8e1a4f7f33
mohibbullahkhan <mohibbullahkhan66@gmail.com>
2026-06-15 02:18:23 +0600

    query 3 complete

--- QUERY.sql
+++ QUERY.sql
@@ -84,3 +84,13 @@ SELECT
 FROM Users
 WHERE full_name ILIKE 'Tanvir%'
    OR full_name ILIKE '%Haque%';
+
+
+--    query 3
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
